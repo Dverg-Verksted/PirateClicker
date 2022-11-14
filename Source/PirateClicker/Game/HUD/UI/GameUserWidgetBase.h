0e189abbe5c32af1885a2c739dbcf80e512a9dc5
Alexander Kuzin <61850604+Por7ableWorlD@users.noreply.github.com>
2022-11-14 10:05:18 +0400

    Update temporary NiagaraUI effect and added base pressed totems buttons logic

--- Source/PirateClicker/Game/HUD/UI/GameUserWidgetBase.h
+++ Source/PirateClicker/Game/HUD/UI/GameUserWidgetBase.h
@@ -9,14 +9,14 @@
 /**
  *
  */
-UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+UENUM(BlueprintType)
 enum class ETotems : uint8
 {
-    NONE = 0 UMETA(Hidden),
-    Fire = 1 << 0,
-    Frost = 1 << 1,
-    ThirdType = 1 << 2,
-    FourthType = 1 << 3,
+    NONE UMETA(Hidden),
+    Fire,
+    Frost,
+    ThirdType,
+    FourthType,
 };
 ENUM_CLASS_FLAGS(ETotems);
 
@@ -24,11 +24,4 @@ UCLASS()
 class PIRATECLICKER_API UGameUserWidgetBase : public UUserWidget
 {
     GENERATED_BODY()
-
-public:
-    //UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Bitmask, BitmaskEnum = "ETotems"))
-    //uint8 TotemFlags = 0;
-
-    UFUNCTION(BlueprintCallable)
-    static  void SetTotemsToHUD(UPARAM(meta = (Bitmask, BitmaskEnum = ETotems)) int32 Totems, bool& Fire, bool& Frost, bool& ThirdType, bool& FourthType);
 };
