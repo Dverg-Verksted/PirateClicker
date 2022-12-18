127d1c56ec8d226f5b66a5f661a0a7c60d5da980
Mark <34418187+markveligod@users.noreply.github.com>
2022-12-18 14:11:03 +0500

    Update script Devops

---
+++ Devops/Clean.bat
@@ -0,0 +1,23 @@
+@echo off
+
+call "%~dp0\config.bat"
+
+set dirsToRemove=Intermediate Saved DerivedDataCache Binaries .vs .idea Script
+
+echo Delete trash directory in project folder
+for %%a in (%dirsToRemove%) do (
+	echo %ProjectRoot%\%%a
+    rmdir /S /Q %ProjectRoot%\%%a
+)
+
+echo Delete trash files
+echo %ProjectRoot%\%ProjectPureName%.sln
+del %ProjectRoot%\%ProjectPureName%.sln
+
+echo Delete trash in Plugins
+for /D %%a in ("%ProjectRoot%\Plugins\*") do (
+	echo %%a\Intermediate
+	echo %%a\Binaries
+   rmdir /S /Q %%a\Intermediate
+   rmdir /S /Q %%a\Binaries
+)
--- Devops/recompileDevelopment.bat
+++ Devops/recompileDevelopment.bat
@@ -2,26 +2,6 @@
 
 call "%~dp0\config.bat"
 
-set dirsToRemove=Intermediate Saved DerivedDataCache Binaries .vs .idea Script
-
-echo Delete trash directory in project folder
-for %%a in (%dirsToRemove%) do (
-	echo %ProjectRoot%\%%a
-    rmdir /S /Q %ProjectRoot%\%%a
-)
-
-echo Delete trash files
-echo %ProjectRoot%\%ProjectPureName%.sln
-del %ProjectRoot%\%ProjectPureName%.sln
-
-echo Delete trash in Plugins
-for /D %%a in ("%ProjectRoot%\Plugins\*") do (
-	echo %%a\Intermediate
-	echo %%a\Binaries
-   rmdir /S /Q %%a\Intermediate
-   rmdir /S /Q %%a\Binaries
-)
-
 "%UBTRelativePath%" -projectfiles -progress -project="%ProjectPath%"
 "%UBTRelativePath%" Development Win64 -Project="%ProjectPath%" -TargetType=Editor -Progress -NoEngineChanges -NoHotReloadFromIDE
 
