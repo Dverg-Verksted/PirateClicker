1ceb8e1d0c8635d6e3f6f62693fb58974db98992
Mark <34418187+markveligod@users.noreply.github.com>
2023-01-19 09:18:01 +0500

    Update to engine UE5.1

--- Plugins/NiagaraUIRenderer/Source/NiagaraUIRenderer/Private/NiagaraSystemWidget.cpp
+++ Plugins/NiagaraUIRenderer/Source/NiagaraUIRenderer/Private/NiagaraSystemWidget.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 - Michal Smoleň
+// Copyright 2022 - Michal Smoleň
 
 #include "NiagaraSystemWidget.h"
 #include "SNiagaraUISystemWidget.h"
@@ -78,9 +78,11 @@ void UNiagaraSystemWidget::InitializeNiagaraUI()
 			
 		if (!NiagaraActor)
 		{
-			FActorSpawnParameters spawnParams;
-			spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-			NiagaraActor = World->SpawnActor<ANiagaraUIActor>(FVector::ZeroVector, FRotator::ZeroRotator, spawnParams);
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+			SpawnParams.ObjectFlags |= RF_Transient;
+			
+			NiagaraActor = World->SpawnActor<ANiagaraUIActor>(FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
 		}
 
 		NiagaraComponent = NiagaraActor->SpawnNewNiagaraUIComponent(NiagaraSystemReference, AutoActivate, ShowDebugSystemInWorld, TickWhenPaused);
